9d9179f631340097f89730e4606ebbd78894d90a
K2kitano02 <k.naoki02@icloud.com>
2026-01-08 20:59:14 +0900

    2nd

--- chapter3.rb
+++ chapter3.rb
@@ -1,5 +1,51 @@
-number = 10
+# number = 10
 
-if number > 5
-  puts "この数値は5より大きいです"
-end
+# if number > 5
+#   puts "この数値は5より大きいです"
+# end
+
+# number = 3
+# if number > 5
+#   puts "この数値は5より大きいです"
+# else
+#   puts "この数値は5以下です"
+# end
+# 
+# number = 5
+# if number > 5
+#   puts "この数値は5より大きいです"
+# elsif number === 5
+#   puts "この数値は5です"
+# else
+#   puts "この数値は5より小さいです"
+# end
+# 
+# if true
+#   puts "この処理は実行されます"
+# end
+# # 
+# if false
+#   puts "この処理は実行されません"
+# end
+
+# # 比較演算子
+# # 
+# a = 10
+# b = 20
+
+# puts a == b
+# puts a != b
+# puts a > b
+# puts a < b
+# puts a => b
+# puts a <= b
+
+# 論理演算子
+# 
+x = true 
+y = false
+
+puts x && y
+puts x || y
+puts !x
+puts !y
